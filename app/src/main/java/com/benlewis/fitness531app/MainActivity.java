280b7973fe7cd17f613e11b3d4b6be4452679807
Ben Lewis <benalewis@hotmail.co.uk>
2016-04-19 19:39:03 +0100

    Started to Think About Implementing 5/3/1
    App is getting very big now, need to cut down on some of the code as
    there is far too much memory on the stack

--- app/src/main/java/com/benlewis/fitness531app/MainActivity.java
+++ app/src/main/java/com/benlewis/fitness531app/MainActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +19,16 @@ public class MainActivity extends AppCompatActivity {
     TextView warmUp1;
     TextView warmUp2;
     TextView warmUp3;
-    Button refreshButton;
-    int OHP1RM;
-    int Bench1RM;
-    int Squat1RM;
-    int Deadlift1RM;
+    TextView fiveThreeOne1;
+    TextView fiveThreeOne2;
+    TextView fiveThreeOne3;
+
+    int ohp1rm;
+    int bench1rm;
+    int squat1rm;
+    int deadlift1rm;
+
+    String liftSpinnerString = "";
 
     static SharedPreferences sharedPreferences;
 
@@ -34,11 +42,35 @@ public class MainActivity extends AppCompatActivity {
         warmUp1 = (TextView) findViewById(R.id.warmup1TextView);
         warmUp2 = (TextView) findViewById(R.id.warmup2TextView);
         warmUp3 = (TextView) findViewById(R.id.warmup3TextView);
-        refreshButton = (Button) findViewById(R.id.mainRefreshButton);
+
+        fiveThreeOne1 = (TextView) findViewById(R.id.main531_1);
+        fiveThreeOne2 = (TextView) findViewById(R.id.main531_2);
+        fiveThreeOne3 = (TextView) findViewById(R.id.main531_3);
+
         sharedPreferences = this.getSharedPreferences("com.benlewis.fitness531app", Context.MODE_PRIVATE);
 
         initPreferences();
         updateLifts();
+
+        //Spinner code
+        Spinner spinner = (Spinner) findViewById(R.id.mainLiftSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.calcArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String r = parent.getItemAtPosition(position).toString();
+                liftSpinnerString = r.toLowerCase();
+                updateLifts();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void initPreferences() {
@@ -54,16 +86,27 @@ public class MainActivity extends AppCompatActivity {
             sharedPreferences.edit().putInt("deadlift", 0).apply();
         }
 
-        OHP1RM = sharedPreferences.getInt("ohp", 0);
-        Bench1RM = sharedPreferences.getInt("bench", 0);
-        Squat1RM = sharedPreferences.getInt("squat", 0);
-        Deadlift1RM = sharedPreferences.getInt("deadlift", 0);
+        ohp1rm = sharedPreferences.getInt("ohp", 0);
+        bench1rm = sharedPreferences.getInt("bench", 0);
+        squat1rm = sharedPreferences.getInt("squat", 0);
+        deadlift1rm = sharedPreferences.getInt("deadlift", 0);
     }
 
     public void updateLifts() {
-        warmUp1.setText(calcWarmUp(OHP1RM, 1));
-        warmUp2.setText(calcWarmUp(OHP1RM, 2));
-        warmUp3.setText(calcWarmUp(OHP1RM, 3));
+        //Called every time the activity is loaded.
+        //Fills the text fields with the correct data
+        switch (liftSpinnerString) {
+            case "ohp":
+                warmUp1.setText(calcWarmUp(ohp1rm, 1));
+                warmUp2.setText(calcWarmUp(ohp1rm, 2));
+                warmUp3.setText(calcWarmUp(ohp1rm, 3));
+                fiveThreeOne1.setText(calc531(ohp1rm, 1));
+                fiveThreeOne2.setText(calc531(ohp1rm, 2));
+                fiveThreeOne3.setText(calc531(ohp1rm, 3));
+                break;
+            default:
+                break;
+        }
     }
 
     public String calcWarmUp(int oneRepMax, int warmUpNumber) {
@@ -84,6 +127,30 @@ public class MainActivity extends AppCompatActivity {
             case 3:
                 adjustment = 0.6;
                 reps = 3;
+                break;
+        }
+
+        warmUp = (((int) (oneRepMax * adjustment)) + " x " + reps);
+        return  warmUp;
+    }
+
+    public String calc531(int oneRepMax, int liftNumber) {
+        double adjustment = 0;
+        int reps = 0;
+        String warmUp = "";
+
+        switch (liftNumber) {
+            case 1:
+                adjustment = 0.75;
+                reps = 5;
+                break;
+            case 2:
+                adjustment = 0.85;
+                reps = 5;
+                break;
+            case 3:
+                adjustment = 0.95;
+                reps = 5;
                 break;
         }
 
